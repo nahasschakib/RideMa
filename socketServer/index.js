3a1c431b81aa1b5b9ec0d9ad8f864810bb79fc55
unknown <cnahass@gmail.com>
2026-06-07 19:13:24 +0100

    fix: CORS socket ouvert pour production Render

--- socketServer/index.js
+++ socketServer/index.js
@@ -22,13 +22,19 @@ const dbConnect =async ()=>{
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-cors:{
-  origin: process.env.NEXT_BASE_URL,
- }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 })
 
 app.post("/emit", async (req,res)=>{
